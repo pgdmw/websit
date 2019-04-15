e4e59086fa3d85ab147ccd61ffa0bc1f9f31f5db
杨晶杰 <yangjingjie@relspace.com.cn>
2019-04-15 14:54:50 +0800

    静态网页

---
+++ websit/src/js/index.js
@@ -0,0 +1,26 @@
+$(function() {
+//     // let num = $(".banner>img").length;
+//     var num = 0;
+//     $(".b-btn").on("click", ".button", function() {
+//         if (num < $(".banner>img").length - 1) {
+//             $(".banner>img").eq(num).animate({ "opacity": "0" }, 500);
+//             num++;
+//             if (num == $(".banner>img").length - 1) {
+//                 $(".button").attr("class", "kkk");
+//             }
+//         }
+//
+//     });
+//     $(".b-btn").on("click", ".kkk", function() {
+//         if (num > 0) {
+//             num--;
+//             $(".banner>img").eq(num).animate({ "opacity": "1" }, 500);
+//             if (num == 0) {
+//                 $(".kkk").attr("class", 'button');
+//             }
+//         }
+//     })
+
+})
+
+
